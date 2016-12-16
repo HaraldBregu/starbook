412ae26aa6ac30aa24e3a1e921a35e3f7bd7736e
harald bregu <harald.bregu@gmail.com>
2016-12-17 00:37:03 +0100

    https redirect

--- app.js
+++ app.js
@@ -5,17 +5,33 @@ var errorHandler = require('errorhandler')
 var bodyParser = require('body-parser')
 var path = require('path')
 var cors = require('cors')
-var config = require('./config')
-var app = express()
+var fs = require('fs')
+var httpsRedirect = require('express-https-redirect');
 var http = require('http')
-var httpServer = http.Server(app)
+var https = require('https')
+var config = require('./config')
+
+var app = express()
+
+
+app.use('/', require('redirect-https')({
+  body: '',
+  trustProxy: true
+}))
+
 
 app.use(cors())
 app.use(bodyParser.json({strict: false}))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(errorHandler({dumpExceptions: true, showStack: true}))
 app.use(express.static(path.join(__dirname, 'angular2app/dist')))
+// app.use('/', httpsRedirect())
+// app.use('/', httpsRedirect(true))
 
-httpServer.listen(config.port, function() {
-	console.log('Server is running on port:' + config.port)
-})
+http.Server(app).listen(config.port)
+
+var options = {
+	key: fs.readFileSync('./cert/key.pem', 'utf8'),
+	cert: fs.readFileSync('./cert/server.crt', 'utf8')
+}
+https.createServer(options, app).listen(443)
