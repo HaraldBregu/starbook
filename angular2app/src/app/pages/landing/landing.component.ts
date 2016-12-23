6c17241ed04d335091de2b32f9b506880d10f5af
aleksandrgorin <podpole@gmail.com>
2016-12-23 03:49:19 +0300

    Added styles for hover on top boxes (landing page). Add action for click on button in top boxes (now open the tab).

--- angular2app/src/app/pages/landing/landing.component.ts
+++ angular2app/src/app/pages/landing/landing.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html'
 })
 export class LandingComponent implements OnInit {
-
+  @Output('tabSelect') activeTab = new EventEmitter();
+  public cardStyles = {1: '0.95', 2: '0.95', 3: '0.9'};
   constructor() { }
 
   ngOnInit() {
   }
+  cardHover(id, type) {
+    if (type === 'on') {
+      this.cardStyles[id] = '1';
+    } else {
+      if (id === 3) {
+        this.cardStyles[id] = '0.9';
+      } else {
+        this.cardStyles[id] = '0.95';
+      }
+    }
+  }
 
+  navigate(tabId) {
+    this.activeTab.emit({id: tabId});
+  }
 }
