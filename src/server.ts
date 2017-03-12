3c146b65f2a901ff9d3b9320248fc7abbb6cbc1c
harald bregu <harald.bregu@gmail.com>
2017-03-12 11:35:41 +0100

    build prod

--- src/server.ts
+++ src/server.ts
@@ -124,13 +124,13 @@ http.createServer(app).listen(port, () => {
 });
 
 if (environment.production) {
-  let options = {
-    key: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
-    cert: fs.readFileSync(__dirname + '/cert/server.crt', 'utf8')
-  };
-  https.createServer(options, app).listen(443, () => {
-    console.log(`Listening on port 443`);
-  });
+  // let options = {
+  //   key: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
+  //   cert: fs.readFileSync(__dirname + '/cert/server.crt', 'utf8')
+  // };
+  // https.createServer(options, app).listen(443, () => {
+  //   console.log(`Listening on port 443`);
+  // });
 }
 
 // app.listen(port, () => {
