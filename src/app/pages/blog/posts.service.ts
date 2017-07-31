a135ce15bd02cbfdece0cf9177165bef5b0651be
harald bregu <harald.bregu@gmail.com>
2017-07-31 11:13:57 +0200

    test

--- src/app/pages/blog/posts.service.ts
+++ src/app/pages/blog/posts.service.ts
@@ -3,80 +3,67 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PostsService {
   public articles = [
-    // {
-    //   "title" : "Crescere la tua impresa edile, facile come fissare un tassello",
-    //   "subtitle" : "Fai anche tu parte dei professionisti che acquistano contatti di potenziali clienti online? Non sei l’unico perché ci sono migliaia di altri professionisti come te che hanno deciso di utilizzare le piattaforme di preventivi online per poter crescere la loro impresa.",
-    //   "picture_url" : "https://pixabay.com/get/e836b2082af6073ecd0b4606e248459ee66ae3d019b0154396f5c17a.jpg",
-    //   "author": {
-    //     "fullname" : "Harald Bregu",
-    //     "headline" : "Sviluppatore, Software, Starbook, Startup",
-    //     "picture_url" : "https://s3-eu-west-1.amazonaws.com/starbook-s3/accounts/595ccfb42bf14e6650ce6e3d/avatar/0"
-    //   },
-    //   "updated_at" : "28 Lug 2017",
-    //   "timing" : "3 min lettura",
-    //   "html_body" : `
-    //   <article>
-    //     <header>
-    //       <h1>Preventivi online? Cosa devi sapere e cosa non ti viene detto</h1>
-    //       <p>
-    //         <time><i class="fa fa-calendar" aria-hidden="true"></i> 28 Lug, 2017</time>
-    //         <time><i class="fa fa-clock-o" aria-hidden="true"></i> 3 min</time>
-    //         <author><i class="fa fa-user" aria-hidden="true"></i> Harald Bregu</author>
-    //       </p>
-    //       <div class="share-links">
-    //         <ul>
-    //         <li class="facebook">
-    //           <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.starbook.co/blog/Preventivi-online%3F-Cosa-devi-sapere-e-cosa-non-ti-viene-detto" title="" target="_blank" rel="nofollow"><i class="fa fa-facebook" aria-hidden="true"></i> <span>Facebook</span></a>
-    //         </li>
-    //         <li class="twitter line">
-    //           <a href="https://twitter.com/home?status=https%3A//www.starbook.co/blog/Preventivi-online%3F-Cosa-devi-sapere-e-cosa-non-ti-viene-detto" title="" target="_blank" rel="nofollow"><i class="fa fa-twitter" aria-hidden="true"></i> <span>Twitter</span></a>
-    //         </li>
-    //         <li class="google line">
-    //           <a href="https://plus.google.com/share?url=https%3A//www.starbook.co/blog/Preventivi-online%3F-Cosa-devi-sapere-e-cosa-non-ti-viene-detto" title="" target="_blank" rel="nofollow"><i class="fa fa-google-plus" aria-hidden="true"></i> <span>Google+</span></a>
-    //         </li>
-    //         <li class="pinterest line">
-    //           <a href="https://pinterest.com/pin/create/button/?url=https%3A//www.starbook.co/blog/Preventivi-online%3F-Cosa-devi-sapere-e-cosa-non-ti-viene-detto&media=https://s3-eu-west-1.amazonaws.com/starbook-s3/blog/preventivi-online-cosa-sapere-e-cosa-non-ti-dicono.jpg&description=Preventivi online? Cosa devi sapere e cosa non ti viene detto" title="" target="_blank" rel="nofollow"><i class="fa fa-pinterest-p" aria-hidden="true"></i> <span>Pinterest</span></a>
-    //         </li>
-    //         <li class="linkedin line">
-    //           <a href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.starbook.co/blog/Preventivi-online%3F-Cosa-devi-sapere-e-cosa-non-ti-viene-detto&title=Preventivi online? Cosa devi sapere e cosa non ti viene detto&summary=Fai anche tu parte dei professionisti che acquistano contatti di potenziali clienti online? Non sei l’unico perché ci sono migliaia di altri professionisti come te che hanno deciso di utilizzare le piattaforme di preventivi online per poter crescere la loro impresa&source=" title="" target="_blank" rel="nofollow"><i class="fa fa-linkedin" aria-hidden="true"></i> <span>LinkedIn</span></a>
-    //         </li>
-    //         </ul>
-    //       </div>
-    //     </header>
-    //     <p>Fai anche tu parte dei professionisti che acquistano contatti di potenziali clienti online? Non sei l’unico perché ci sono migliaia di altri professionisti come te che hanno deciso di utilizzare le piattaforme di preventivi online per poter crescere la loro impresa.</p>
-    //     <p><img src="https://pixabay.com/get/ec36b8092dfd1c3e81594601e24f4e93fe76e7d518b317409cf9c1.jpg" alt="Gli artigiani sul web" title="Gli artigiani sul web"/></p>
-    //     <p>Molti professionisti hanno deciso di dare una svolta nella crescita della loro azienda, che sia di costruzioni, pulizia, edilizia o idraulica. Tuttavia ci sono cose che devi conoscere prima di iniziare ad utilizzarli e prima di spendere soldi inutilmente.</p>
-    //     <section>
-    //       <h2>Il funzionamento</h2>
-    //       <p>Come abbiamo già parlato anche in un <a href="/blog/Gli-artigiani-del-web%3A-3-semplici-modi-per-aumentare-i-clienti-online" target="_blank">altro articolo</a> su come aumentare i clienti online, il funzionamento di questi siti è semplice. Ti iscrivi come impresa, inserisci la P.IVA, selezioni la tua professione e inizi a ricevere offerte di lavoro. Ogni volta che c’è una nuova richiesta di preventivo ti arriva una notifica email e un SMS. A quel punto decidi di comprare il contatto e d’inviarli un preventivo. I prezzi variano dai 5 ai 20€ per ogni preventivo o contatto acquistato. Dal altra parte il cliente riceve fino a 5 preventivi da altri professionisti e imprese che hanno acquistato il contatto come te.</p>
-    //     </section>
-    //     <section>
-    //       <h2>Problema</h2>
-    //       <p>Analizziamo i vantaggi e svantaggi. Questi sistemi sono ottimi per trovare contatti e trasformarli in clienti, ma non è detto che il contatto sia reale o veramente interessato. Facendo due conti per ogni preventivo solo 1 su 5 delle imprese vince il lavoro. Quindi in media dovresti fare 5 preventivi per vincere uno. Non solo vincerlo ma anche assicurarti che il cliente sia regolare, che non cambi idea al ultimo minuto è che abbia chiaro la data del inizio dei lavori. Riassumendo in punti i problemi sono:</p>
-    //       <ul>
-    //         <li><strong>Nessuna garanzia di lavoro</strong></li>
-    //         <li><strong>Alto tasso d'insuccesso</strong></li>
-    //         <li><strong>Clienti non interessati</strong></li>
-    //         <li><strong>Molta concorrenza</strong></li>
-    //         <li><strong>Facilmente adatto alle truffe</strong></li>
-    //       </ul>
-    //     </section>
-    //     <section>
-    //       <h2>Soluzione</h2>
-    //       <p>La soluzione non è standard per tutte le imprese che acquistano contatti online. Si potrebbero trovare centinaia di soluzioni validi, ma visto che i sistemi di cui parliamo sono online, le soluzioni si focalizzano in quel contesto. In sintesi le soluzioni sono:</p>
-    //       <ul>
-    //         <li><strong>Verifica dei contatti acquistati</strong></li>
-    //         <li><strong>Controllare l'interessamento del cliente</strong></li>
-    //         <li><strong>Inviare email professionali</strong></li>
-    //         <li><strong>Inviare SMS professionali</strong></li>
-    //         <li><strong>Aumentare il successo dei preventivi online</strong></li>
-    //         <li><strong>Diventare competitivi</strong></li>
-    //       </ul>
-    //       <p>Questi sono delle soluzioni validi per verificare i vostri clienti, crescere il successo dei preventivi, chiedere rimborsi e ovviamente crescere i guadagni.</p>
-    //     </section>
-    //   </article>
-    //   `
-    // },
+    {
+      "title" : "Crescere la tua impresa online, facile come fissare un tassello",
+      "subtitle" : "Ci sono due scenari: primo che hai un impresa edile giovane e che vuoi farla crescere, secondo è che hai già un impresa edile ma che è in crisi e non hai fatturato abbastanza l’ultimo anno. La soluzione è semplice, investi online per incrementare il fatturato e crescere la tua impresa.",
+      "picture_url" : "https://s3-eu-west-1.amazonaws.com/starbook-s3/blog/crescere-la-tua-impresa-online-facile-come-fissare-un-tassello.jpg",
+      "author": {
+        "fullname" : "Harald Bregu",
+        "headline" : "Sviluppatore, Software, Starbook, Startup",
+        "picture_url" : "https://s3-eu-west-1.amazonaws.com/starbook-s3/accounts/595ccfb42bf14e6650ce6e3d/avatar/0"
+      },
+      "updated_at" : "31 Lug 2017",
+      "timing" : "2 min lettura",
+      "html_body" : `
+      <article>
+        <header>
+          <h1>Crescere la tua impresa online, facile come fissare un tassello</h1>
+          <p>
+            <time><i class="fa fa-calendar" aria-hidden="true"></i> 31 Lug, 2017</time>
+            <time><i class="fa fa-clock-o" aria-hidden="true"></i> 2 min</time>
+            <author><i class="fa fa-user" aria-hidden="true"></i> Harald Bregu</author>
+          </p>
+          <div class="share-links">
+            <ul>
+            <li class="facebook">
+              <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.starbook.co/blog/Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello" title="" target="_blank" rel="nofollow"><i class="fa fa-facebook" aria-hidden="true"></i> <span>Facebook</span></a>
+            </li>
+            <li class="twitter line">
+              <a href="https://twitter.com/home?status=https%3A//www.starbook.co/blog/Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello" title="" target="_blank" rel="nofollow"><i class="fa fa-twitter" aria-hidden="true"></i> <span>Twitter</span></a>
+            </li>
+            <li class="google line">
+              <a href="https://plus.google.com/share?url=https%3A//www.starbook.co/blog/Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello" title="" target="_blank" rel="nofollow"><i class="fa fa-google-plus" aria-hidden="true"></i> <span>Google+</span></a>
+            </li>
+            <li class="pinterest line">
+              <a href="https://pinterest.com/pin/create/button/?url=https%3A//www.starbook.co/blog/Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello&media=https://s3-eu-west-1.amazonaws.com/starbook-s3/blog/crescere-la-tua-impresa-online-facile-come-fissare-un-tassello.jpg&description=Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello" title="" target="_blank" rel="nofollow"><i class="fa fa-pinterest-p" aria-hidden="true"></i> <span>Pinterest</span></a>
+            </li>
+            <li class="linkedin line">
+              <a href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.starbook.co/blog/Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello&title=Crescere-la-tua-impresa-online%2C-facile-come-fissare-un-tassello&summary=Ci sono due scenari: primo che hai un impresa edile giovane e che vuoi farla crescere, secondo è che hai già un impresa edile ma che è in crisi e non hai fatturato abbastanza l’ultimo anno. La soluzione è semplice, investi online per incrementare il fatturato e crescere la tua impresa&source=" title="" target="_blank" rel="nofollow"><i class="fa fa-linkedin" aria-hidden="true"></i> <span>LinkedIn</span></a>
+            </li>
+            </ul>
+          </div>
+        </header>
+        <p>Ci sono <strong>due</strong> scenari: <strong>primo</strong> che hai un impresa edile giovane e che vuoi farla crescere, <strong>secondo</strong> è che hai già un impresa edile ma che è in crisi e non hai fatturato abbastanza l’ultimo anno. La soluzione è semplice, investi online per incrementare il fatturato e crescere la tua impresa.</p>
+        <p><img src="https://s3-eu-west-1.amazonaws.com/starbook-s3/blog/crescere-la-tua-impresa-online-facile-come-fissare-un-tassello.jpg" alt="Gli artigiani sul web" title="Gli artigiani sul web"/></p>
+        <p>Pochi sono quelli che utilizzano il web come uno strumento per poter crescere la propria attività e in questo caso la loro impresa edile. Proprio per questo fatto solo chi sarà tra i primi a utilizzare il web nel modo corretto avrà i benefici. Non pensarci troppo, mettiti seduto per 2 minuti, leggi questo articolo e scopri perché il web e la tua salvezza.</p>
+        <section>
+          <h2>Più web, più clienti</h2>
+          <p>Il web non è solo uno strumento di divertimento. Si possono trovare <strong>migliaia di clienti</strong> utilizzando i canali giusti. Nel web non hai bisogno di essere conosciuto per poter trovare un cliente, ti basta migliorare la tua <strong>immagine aziendale</strong> e <strong>tecniche di marketing</strong> per farlo. Adesso i consumatori sono più fiduciari nei sistemi digitali. Comprano, prenotano senza avere il timore di essere truffati. Il web stesso è più sicuro che mai, le transazioni sono più sicure. Quindi offrire i tuoi servizi online è già un <strong>vantaggio competitivo</strong> che dovresti avere.</p>
+        </section>
+        <section>
+          <h2>Poca concorrenza</h2>
+          <p>Essendo che il web venga utilizzato da pochi impresari, la <strong>concorrenza è minima</strong> ed è un altro motivo perché dovresti investire per trovare clienti online invece che continuare con i sistemi tradizionali. La concorrenza non lo batterai mai con la qualità del tuo lavoro ma innovandoti giorno dopo giorno, nuovi sistemi di promozione ogni giorno e avrai sempre <strong>nuove opportunità</strong>.</p>
+        </section>
+        <section>
+          <h2>Prezzo giusto ma più guadagni</h2>
+          <p>Un altro motivo perché dovresti utilizzare il web per promuovere la tua attività è che non devi fare <strong>troppi sconti</strong>. Se sei unico sei anche prezioso e quindi il cliente non ha molta scelta. L’innovazione ti permette di essere <strong>all’avanguardia</strong> e di decidere anche il prezzo. Questo ti permette non solo di lavorare nel modo giusto ma anche di <strong>crescere i guadagni</strong> e il fatturato.</p>
+        </section>
+        <section>
+          <h2>Vuoi sapere di più? Inviami la tua email qui sotto.</h2>
+        </section>
+      </article>
+      `
+    },
     {
       "title" : "Preventivi online? Cosa devi sapere e cosa non ti viene detto",
       "subtitle" : "Fai anche tu parte dei professionisti che acquistano contatti di potenziali clienti online? Non sei l’unico perché ci sono migliaia di altri professionisti come te che hanno deciso di utilizzare le piattaforme di preventivi online per poter crescere la loro impresa.",
