a2963f6bca5304f6df3a415f78733f6d265c3120
harald bregu <harald.bregu@gmail.com>
2017-08-01 18:43:59 +0200

    test

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -83,8 +83,12 @@ export class CompanyComponent implements OnInit {
 
     if (isBrowser) {
       let left = Math.round((document.documentElement.clientWidth / 2) - 285);
-      let facebookPopup = window.open('https://www.facebook.com/v2.8/dialog/oauth?client_id=1108461325907277&response_type=token&scope=email,public_profile,user_location,user_website,user_work_history&redirect_uri=https://glacial-shore-66987.herokuapp.com/facebook', '_blank', 'location=yes,height=570,width=520,left=' + left + ', top=100,scrollbars=yes,status=yes')
+
+      // TEST
+      let facebookPopup = window.open('https://www.facebook.com/v2.8/dialog/oauth?client_id=1251898728230202&response_type=token&scope=email,public_profile,user_location,user_website,user_work_history&redirect_uri=https://glacial-shore-66987.herokuapp.com/facebook', '_blank', 'location=yes,height=570,width=520,left=' + left + ', top=100,scrollbars=yes,status=yes')
       // let facebookPopup = window.open('https://www.facebook.com/v2.8/dialog/oauth?client_id=1251898728230202&response_type=token&scope=email,public_profile,user_location,user_website,user_work_history&redirect_uri=http://localhost:4200/facebook', '_blank', 'location=yes,height=570,width=520,left=' + left + ', top=100,scrollbars=yes,status=yes')
+
+      // PROD
       // let facebookPopup = window.open('https://www.facebook.com/v2.8/dialog/oauth?client_id=1108461325907277&response_type=token&scope=email,public_profile,user_location,user_website,user_work_history&redirect_uri=http://www.starbook.co/facebook', '_blank', 'location=yes,height=570,width=520,left=' + left + ', top=100,scrollbars=yes,status=yes')
       this.checkAccessToken(facebookPopup, 1);
     }
