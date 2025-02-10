32a96d5f7bee774c7cd524c31646e0d5ad3d937f
harald bregu <harald.bregu@gmail.com>
2025-02-10 13:46:42 +0100

    new version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,29 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    });
-    TestBed.compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
   });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  }));
+  });
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  it(`should have the 'starbook_app' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('starbook_app');
+  });
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, starbook_app');
+  });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,172 +1,66 @@
+import {
+  AngularNodeAppEngine,
+  createNodeRequestHandler,
+  isMainModule,
+  writeResponseToNodeResponse,
+} from '@angular/ssr/node';
+import express from 'express';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = resolve(serverDistFolder, '../browser');
+
+const app = express();
+const angularApp = new AngularNodeAppEngine();
+
 /**
- * the polyfills must be the first thing imported
+ * Example Express Rest API endpoints can be defined here.
+ * Uncomment and define endpoints as necessary.
+ *
+ * Example:
+ * ```ts
+ * app.get('/api/**', (req, res) => {
+ *   // Handle API request
+ * });
+ * ```
  */
-import './polyfills.ts';
-import './__2.1.1.workaround.ts'; // temporary until 2.1.1 things are patched in Core
-import './__workaround.node';
-import * as path from 'path';
-import * as express from 'express';
-import * as compression from 'compression';
-import * as fs from 'fs';
-import * as http from 'http';
-import * as https from 'https';
-import * as cors from "cors";
-
-import { createEngine } from 'angular2-express-engine';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.node.module';
-import { environment } from './environments/environment';
-import { routes } from './server.routes';
-
-// App
-const app  = express();
-const ROOT = path.join(path.resolve(__dirname, '..'));
-const port = process.env.PORT || 80 || 8080;
-// const port = 4200;
-// sitemap.generate(app);
 
 /**
- * enable prod mode for production environments
+ * Serve static files from /browser
  */
-// if (environment.production) {
-//   enableProdMode();
-// }
-// console.log('env is: ' + JSON.stringify(environment));
+app.use(
+  express.static(browserDistFolder, {
+    maxAge: '1y',
+    index: false,
+    redirect: false,
+  }),
+);
 
 /**
- * Express View
+ * Handle all other requests by rendering the Angular application.
  */
-app.engine('.html', createEngine({}));
-app.set('views', path.join(ROOT, 'client'));
-app.set('view engine', 'html');
+app.use('/**', (req, res, next) => {
+  angularApp
+    .handle(req)
+    .then((response) =>
+      response ? writeResponseToNodeResponse(response, res) : next(),
+    )
+    .catch(next);
+});
 
 /**
- *cors
-*/
-// app.use(cors())
-//options for cors midddleware
-// const options:cors.CorsOptions = {
-//   allowedHeaders: ["Access-Control-Allow-Origin"],
-//   // credentials: true,
-//   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-//   // origin: API_URL,
-//   preflightContinue: false
-// };
-// //use cors middleware
-// app.use(cors(options));
-//
-// //add your routes
-//
-// //enable pre-flight
-// app.options("*", cors(options));
-
-
-/**
- * Enable compression
+ * Start the server if this module is the main entry point.
+ * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-app.use(compression());
-// app.use(function(req, res) {
-//   return res.redirect('https://' + req.hostname + req.originalUrl);
-// })
-/**
- * serve static files
- */
-app.use('/', express.static(path.join(ROOT, 'client'), {index: false}));
-
-/* There are code from old server */
-if (environment.production) {
-  app.all('/*', function(req: any, res: any, next) {
-    if(!/^www\./.test(req.headers.host) && req.headers.host === "starbook.co") {
-      res.status(301).redirect(req.protocol + '://www.' + req.headers.host + req.url)
-    } else {
-      next()
-    }
-  })
-  app.use('/', require('redirect-https')({ body: '', port: 443, trustProxy: true }))
-} else {
-  // app.all('/*', function(req: any, res: any, next) {
-  //   res.status(301).redirect('https://www.' + req.headers.host + req.url)
-  // })
-
-  // app.use('*',function(req,res){
-  //   res.redirect('https://www.starbook.co'+req.url)
-  // })
-
-  // app.use('/', require('redirect-https')({
-  //   body: '',
-  //   port: 443,
-  //   trustProxy: true
-  // }))
-  // app.all('/*', function(req: any, res: any, next) {
-  //   console.log('host: ' + req.headers.host);
-  //   console.log('url: ' + JSON.stringify(req.url));
-  //   if (!/https/.test(req.protocol) && (req.headers.host === "localhost:4200")){
-  //     res.redirect("http://" + req.headers.host + req.url);
-  //   } else {
-  //     next()
-  //   }
-  // })
-}
-
-
-/**
- * bootstrap universal app
- * @param req
- * @param res
- */
-function ngApp(req: any, res: any) {
-  // console.log('protocol: ' + req.protocol);
-  // console.log('host: ' + req.headers.host);
-  // console.log('url: ' + JSON.stringify(req.url));
-  res.render('index', {
-    req,
-    res,
-    ngModule: AppModule,
-    preboot: false,
-    baseUrl: '/',
-    requestUrl: req.originalUrl,
-    originUrl: req.hostname
+if (isMainModule(import.meta.url)) {
+  const port = process.env['PORT'] || 4000;
+  app.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
 /**
- * use universal for specific routes
+ * The request handler used by the Angular CLI (dev-server and during build).
  */
-app.get('/', ngApp);
-app.get('/robots.txt', function(req, res) {
-  res.type('text/plain')
-  res.send("User-agent: *\nDisallow: \nSitemap: https://www.starbook.co/sitemap.xml");
-})
-app.get('/sitemap.xml', function(req, res) {
-  res.type('text/xml')
-  res.send("<?xml version='1.0' encoding='UTF-8'?><urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd'><url><loc>https://www.starbook.co/</loc></url><url><loc>https://www.starbook.co/company</loc></url><url><loc>https://www.starbook.co/services</loc></url><url><loc>https://www.starbook.co/info/about</loc></url><url><loc>https://www.starbook.co/info/help</loc></url><url><loc>https://www.starbook.co/info/legal</loc></url><url><loc>https://www.starbook.co/auth/worker</loc></url><url><loc>https://www.starbook.co/auth/login</loc></url><url><loc>https://www.starbook.co/insert/product</loc></url><url><loc>https://www.starbook.co/blog</loc></url><url><loc>https://www.starbook.co/business</loc></url></urlset>");
-})
-routes.forEach(route => {
-  app.get(`/${route}`, ngApp);
-  app.get(`/${route}/*`, ngApp);
-});
-
-/**
- * if you want to use universal for all routes, you can use the '*' wildcard
- */
-app.get('*', function (req: any, res: any) {
-  res.setHeader('Content-Type', 'application/json');
-  const pojo = {status: 404, message: 'No Content'};
-  const json = JSON.stringify(pojo, null, 2);
-  res.status(404).send(json);
-});
-
-/* There are code from old server (app.js) */
-http.createServer(app).listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-if (environment.production) { // This do not work for heroku
-  let options = {
-    key: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
-    cert: fs.readFileSync(__dirname + '/cert/server.crt', 'utf8')
-  };
-  https.createServer(options, app).listen(443, () => {
-    console.log(`Listening on port 443`);
-  });
-}
+export const reqHandler = createNodeRequestHandler(app);
